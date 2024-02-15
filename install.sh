0a7f3283f4f38d574f471ad6346605b58245e45b
tregu0458 <54461561+tregu148@users.noreply.github.com>
2024-02-15 18:47:56 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ LF=$(printf '\\\012_')
 LF=${LF%_}
 EXTENSION_NAME=ChatGPT2Scratch
 EXTENSION_ID=chatgpt2scratch
-COLLABORATOR=tregu148
+COLLABORATOR=ichiroc,tregu148
 EXTENSION_DESCRIPTION="ChatGPT for Scratch"
 
 mkdir -p node_modules/scratch-vm/src/extensions/scratch3_${EXTENSION_ID}
